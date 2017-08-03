6f16084733ef1d1338c5fb13ea203e7a5d47137e
Thanh Phuc <phucsecond6554@gmail.com>
2017-08-03 21:38:39 +0700

    Khong co gi chi la chinh sua mot it

--- text-editor.js
+++ text-editor.js
@@ -10,7 +10,8 @@
     var html = document.createElement("button"); // Nut hien html code
     var tag = document.createElement("button");
     var link = document.createElement("button");
-    
+		var image = document.createElement("button");
+
     //Dua cac element vao trang
     texteditor.appendChild(frame);
     texteditor.appendChild(area);
@@ -19,55 +20,68 @@
     texteditor.appendChild(html);
     texteditor.appendChild(tag);
     texteditor.appendChild(link);
-    
+		texteditor.appendChild(image);
+
+
+		//Add CSS style cho frame editor
+		var style = document.createElement("link");
+		style.setAttribute("rel","stylesheet");
+		style.setAttribute("href","style.css");
+		frame.contentWindow.document.head.appendChild(style);
+
     //Frame chua text editor
-    
+
     frame.setAttribute("id", "textframe");
     frame.style.width = "100%";
-    frame.style.height = "200px";
+    frame.style.height = "300px";
     frame.contentWindow.document.designMode = "on";
-    
+
     //Text area chua html code
     area.setAttribute("id","htmlArea");
     area.setAttribute("disabled","true");
+		area.setAttribute("name", "text_content");
     area.style.width = "100%";
-    area.style.height = "200px";
+    area.style.height = "300px";
     area.style.display = "none";
-    
+
     //Button Bold
     bold.innerHTML = "Bold";
     bold.setAttribute("id", "bold_btn");
-    
+
     // Italic button
     italic.innerHTML = "Italic";
     italic.setAttribute("id","italic_btn");
-    
+
     //Text Button
     html.innerHTML = "HTML";
     html.setAttribute("id","text_btn");
-    
+
     //Tag Button
     tag.innerHTML = "Tag";
     tag.setAttribute("id", "tag_btn");
-    
+
     //Link button
     link.innerHTML = "Link";
     link.setAttribute("id","link_btn");
-    
+
+		//Image Button
+		image.innerHTML = "Image";
+		image.setAttribute("id","image_btn");
+
     bold.onclick = function(){
         let edit = frame.contentWindow;
         edit.focus();
         edit.document.execCommand("bold","false","");
         edit.focus();
     } // Khi nhan nut bold se in dam van ban
-    
+
     italic.onclick = function(){
         let edit = frame.contentWindow;
         edit.focus();
         edit.document.execCommand("italic","false","");
         edit.focus();
     } // KHi nhan vao nut Italic se in nghieng van ban
-    
+
     var html_toggle = false;
     html.onclick = function(){
         if(html_toggle == false){
@@ -79,15 +93,15 @@
             area.style.display = "none";
             html_toggle = false;
         }
-    } // Chuyen doi text <-> html 
-    
+    } // Chuyen doi text <-> html
+
     tag.onclick = function(){
         let tag_content = prompt("Nhap tag ban muon","Example: <h1>this is h1 </h1>");
         let edit = frame.contentWindow.document;
         if(tag_content !== null)
             edit.body.innerHTML += tag_content;
     }
-    
+
     link.onclick = function(){
         let link_site = prompt("Nhap link","http://");
         let edit = frame.contentWindow.document;
@@ -95,9 +109,17 @@
             let a_tag = "<a href="+link_site+">"+link_site+"</a>";
             edit.body.innerHTML += a_tag;
         }
-            
-    }
-    
+    }//Insert a link tag
+
+		image.onclick = function(){
+			let img_link = prompt("Image Link", "http://");
+			let edit = frame.contentWindow.document;
+			if(img_link !== null){
+				let img_tag = "<img src="+img_link+">";
+				edit.body.innerHTML += img_tag;
+			}
+		}
+
     setInterval(function(){
         var edit = frame.contentWindow.document;
         area.value = edit.body.innerHTML;
